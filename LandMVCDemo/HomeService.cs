e5409df83c0e84fbc067f2cd5063744586b4220c
gaocong <718497701@qq.com>
2017-07-16 22:27:04 +0800

    添加示例项目

---
+++ LandMVCDemo/HomeService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LandMVCDemo
+{
+    public class HomeService
+    {
+        //Get /Home/helloWorld
+        public string helloWorld()
+        {
+            return "你好,世界！";
+        }
+    }
+}
